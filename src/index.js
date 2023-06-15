7e2ee16838960ec4dec6defa99294ca8186c3643
albertPapp <78800364+albertPapp@users.noreply.github.com>
2023-06-16 00:30:48 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import "./styles.css";
+
+const table = document.getElementById("table");
+
+async function getData() {
+  const url =
+    "https://statfin.stat.fi/PxWeb/sq/4e244893-7761-4c4f-8e55-7a8d41d86eff";
+  const dataPromise = await fetch(url);
+  const dataJson = await dataPromise.json();
+
+  const employmentUrl =
+    "https://statfin.stat.fi/PxWeb/sq/5e288b40-f8c8-4f1e-b3b0-61b86ce5c065";
+  const emplymentPromise = await fetch(employmentUrl);
+  const employmentJson = await emplymentPromise.json();
+  console.log(employmentJson);
+
+  const municipality = Object.values(
+    dataJson.dataset.dimension.Alue.category.label
+  );
+  const population = dataJson.dataset.value;
+  const employment = employmentJson.dataset.value;
+
+  for (let i = 0; i < 10; i++) {
+    let tr = document.createElement("tr");
+    let td1 = document.createElement("td");
+    let td2 = document.createElement("td");
+    let td3 = document.createElement("td");
+
+    td1.innerHTML = municipality[i];
+    td2.innerHTML = population[i];
+    td3.innerText = employment[i];
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    table.appendChild(tr);
+  }
+}
+
+getData();
